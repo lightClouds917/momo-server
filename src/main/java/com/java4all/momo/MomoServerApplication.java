89e6ce465bdf7b06be7df50f92f83307460e7c6a
ITyunqing <1186355422@qq.com>
2019-09-30 09:56:53 +0800

    init server

---
+++ src/main/java/com/java4all/momo/MomoServerApplication.java
@@ -0,0 +1,13 @@
+package com.java4all.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoServerApplication.class, args);
+	}
+
+}
